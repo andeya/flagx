8258ae927484f7c9010505962a5119ef6dc9e202
henrylee2cn <henrylee2cn@gmail.com>
2020-01-07 11:45:12 +0800

    style: optimize code
    Change-Id: I166cd831100d4bd43ff117898cbbbb3e8e54f87f

--- flagx.go
+++ flagx.go
@@ -5,27 +5,21 @@ import (
 	"strings"
 )
 
-// ErrorHandling defines how FlagSet.Parse behaves if the parse fails.
-type ErrorHandling = flag.ErrorHandling
-
-// These constants cause FlagSet.Parse to behave as described if the parse fails.
-const (
-	ContinueOnError     ErrorHandling = flag.ContinueOnError // Return a descriptive error.
-	ExitOnError         ErrorHandling = flag.ExitOnError     // Call os.Exit(2).
-	PanicOnError        ErrorHandling = flag.PanicOnError    // Call panic with a descriptive error.
-	ContinueOnUndefined ErrorHandling = 1 << 30              // Ignore provided but undefined flags
-)
-
-// A FlagSet represents a set of defined flags. The zero value of a FlagSet
-// has no name and has ContinueOnError error handling.
-type FlagSet struct {
-	*flag.FlagSet
-	errorHandling         ErrorHandling
-	isContinueOnUndefined bool
-}
 type (
+	// ErrorHandling defines how FlagSet.Parse behaves if the parse fails.
+	ErrorHandling = flag.ErrorHandling
+
+	// A FlagSet represents a set of defined flags. The zero value of a FlagSet
+	// has no name and has ContinueOnError error handling.
+	FlagSet struct {
+		*flag.FlagSet
+		errorHandling         ErrorHandling
+		isContinueOnUndefined bool
+	}
+
 	// A Flag represents the state of a flag.
 	Flag = flag.Flag
+
 	// Getter is an interface that allows the contents of a Value to be retrieved.
 	// It wraps the Value interface, rather than being part of it, because it
 	// appeared after Go 1 and its compatibility rules. All Value types provided
@@ -45,6 +39,14 @@ type (
 	Value = flag.Value
 )
 
+// These constants cause FlagSet.Parse to behave as described if the parse fails.
+const (
+	ContinueOnError     ErrorHandling = flag.ContinueOnError // Return a descriptive error.
+	ExitOnError         ErrorHandling = flag.ExitOnError     // Call os.Exit(2).
+	PanicOnError        ErrorHandling = flag.PanicOnError    // Call panic with a descriptive error.
+	ContinueOnUndefined ErrorHandling = 1 << 30              // Ignore provided but undefined flags
+)
+
 // NewFlagSet returns a new, empty flag set with the specified name and
 // error handling property. If the name is not empty, it will be printed
 // in the default usage message and in error messages.
@@ -54,11 +56,6 @@ func NewFlagSet(name string, errorHandling ErrorHandling) *FlagSet {
 	errorHandling, fs.isContinueOnUndefined = cleanBit(errorHandling, ContinueOnUndefined)
 	fs.FlagSet = flag.NewFlagSet(name, errorHandling)
 	return fs
-}
-
-func cleanBit(eh, bit ErrorHandling) (ErrorHandling, bool) {
-	eh2 := eh &^ bit
-	return eh2, eh2 != eh
 }
 
 // Parse parses flag definitions from the argument list, which should not
@@ -108,4 +105,9 @@ L:
 		}
 	}
 	return a
+}
+
+func cleanBit(eh, bit ErrorHandling) (ErrorHandling, bool) {
+	eh2 := eh &^ bit
+	return eh2, eh2 != eh
 }
